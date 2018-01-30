349824e9964d44c5e52e2ec0545cc15043ef4394
MynahMarie <mynah.marie@gmail.com>
2018-01-30 20:44:32 +0200

    Fix problem with cluster in local storage as well as basic CSS

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Editor } from './components/Editor';
 
 export const App = (props) => (
-  <div className="container">
+  <div className="container blue-grey lighten-5">
     <Editor noteRoot={props.noteRoot}/>
   </div>
 );
