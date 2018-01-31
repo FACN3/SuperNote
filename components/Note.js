948268f85eadf551882ec3d850a2bd555900874a
hasansaad1 <hasansaad.saad1998@gmail.com>
2018-01-31 11:15:29 +0200

    fixed the raised issues relates #8 #9

--- components/Note.js
+++ components/Note.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Component } from 'react';
-import 'materialize-css/dist/css/materialize.css';
+import React from "react";
+import { Component } from "react";
+import "materialize-css/dist/css/materialize.css";
 
 export class Note extends Component {
   state = {
@@ -9,68 +9,77 @@ export class Note extends Component {
     newTitle: this.props.title,
     newDescription: this.props.description
   };
-  delete = (e) => {
-      this.setState({showNote: false})
-      let myStorage = JSON.parse(localStorage.getItem('notes'));
-      myStorage[this.props.index] = {};
-      localStorage.setItem('notes', JSON.stringify(myStorage));
+  delete = () => {
+    this.setState({ showNote: false });
+    let myStorage = JSON.parse(localStorage.getItem("notes"));
+    myStorage[this.props.index] = {};
+    localStorage.setItem("notes", JSON.stringify(myStorage));
   };
-  editing = (e) => {
-    this.setState({editing: true})
+  editing = () => {
+    this.setState({ editing: true });
   };
-  recordEdits = (e) => {
+  recordEdits = e => {
     if (e.target.name === "title") {
       this.setState({
-          newTitle: e.target.value
-      })
-    }
-    else {
+        newTitle: e.target.value
+      });
+    } else {
       this.setState({
         newDescription: e.target.value
-      })
+      });
     }
   };
   saveEdit = () => {
-    this.setState({editing: false});
+    this.setState({ editing: false });
     let note = {
       title: this.state.newTitle,
       description: this.state.newDescription
     };
-    let myStorage = JSON.parse(localStorage.getItem('notes'));
+    let myStorage = JSON.parse(localStorage.getItem("notes"));
     myStorage[this.props.index] = note;
-    localStorage.setItem('notes', JSON.stringify(myStorage));
+    localStorage.setItem("notes", JSON.stringify(myStorage));
   };
   render() {
     if (this.state.showNote && !this.state.editing) {
-    return (
-      <div className="row">
-        <div className="col s12 m6">
-          <div className="card blue-grey darken-1">
-            <div className="card-content white-text">
-              <span className="card-title">{this.state.newTitle}</span>
-              <p>{this.state.newDescription}</p>
-            </div>
-            <div className="card-action">
-              <button className="delete" onClick={this.delete}>Delete</button>
-              <button className="edit" onClick={this.editing}>Edit</button>
+      return (
+        <div className="row">
+          <div className="col s12 m6">
+            <div className="card blue-grey darken-1">
+              <div className="card-content white-text break_word">
+                <span className="card-title">{this.state.newTitle}</span>
+                <p>{this.state.newDescription}</p>
+              </div>
+              <div className="card-action">
+                <button className="delete" onClick={this.delete}>
+                  Delete
+                </button>
+                <button className="edit" onClick={this.editing}>
+                  Edit
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    );
-  }
-  else if (this.state.editing) {
+      );
+    } else if (this.state.editing) {
       return (
         <div className="row">
           <div className="col s12 m6">
             <div className="card blue-grey darken-1">
               <div className="card-content white-text">
-                <input name="title" onChange={this.recordEdits} type="text"
-                  className="card-title" value={this.state.newTitle}>
-                  </input>
-                <input name="description" onChange={this.recordEdits} type="text"
-                   value={this.state.newDescription}>
-                   </input>
+                <input
+                  name="title"
+                  onChange={this.recordEdits}
+                  type="text"
+                  className="card-title"
+                  value={this.state.newTitle}
+                />
+                <input
+                  name="description"
+                  onChange={this.recordEdits}
+                  type="text"
+                  value={this.state.newDescription}
+                />
               </div>
               <div className="card-action">
                 <button onClick={this.saveEdit}>Save</button>
@@ -79,12 +88,8 @@ export class Note extends Component {
           </div>
         </div>
       );
+    } else {
+      return <div />;
     }
-  else {
-    return (
-      <div>
-      </div>
-    );
-  }
   }
 }
